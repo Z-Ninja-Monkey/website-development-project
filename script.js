030447166804d601d3bce44e0e51b51302daa46c
Anson <100244371+Z-Ninja-Monkey@users.noreply.github.com>
2024-11-04 21:02:11 +0000

    Fixed navbar bug and added coming soon

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $("a").on("click", function () {
-
+    
     let car = $(this).children().first().html();
-    alert("You are buying a " + car + " for 100,000,000 dollars!");
-    alert("Confirm?");
+    
+    if (car == "Lamborghini Temerario" || car == "Porsche 911 Turbo S" || car == "Bugatti Chiron") {
 
+        alert("You are buying a " + car + " for 100,000,000 dollars!");
+        alert("Confirm?");
+
+    }
+    
 })
